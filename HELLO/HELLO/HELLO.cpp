bcff3907c12fcebd75bf2bd0fa70340e4c9d2a1f
Brandon Hawkins <hawkinsb@sou.edu>
2013-04-05 18:38:38 -0800

    Helloworld

---
+++ HELLO/HELLO/HELLO.cpp
@@ -0,0 +1,14 @@
+// HELLO.cpp : Defines the entry point for the console application.
+//Brandon Hawkins
+//helloworld
+
+#include "stdafx.h"
+#include<iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	using namespace std;
+	cout<<"helloworld"<<endl;
+	return 0;
+}
+
